9c5858e2d7c612445c51c547b49fb07d23d0c4b5
Carlos <guerrerocarlos@gmail.com>
2016-10-17 16:42:45 +0300

    Initial working prototype :)

---
+++ assets/js/cachep2p.security.js
@@ -0,0 +1,6 @@
+document.security_sha1 = {
+    'http://cachep2p.com/':                       '8a01e1d8c19294d6ad9a4af20cdab1798e4111b2',
+    'http://www.cachep2p.com/':                   '8a01e1d8c19294d6ad9a4af20cdab1798e4111b2',
+    'http://cachep2p.com/documentation.html':     '714194b113eedd8ba76b9fa0759686869088d1c3',
+    'http://www.cachep2p.com/documentation.html': '714194b113eedd8ba76b9fa0759686869088d1c3'
+};
---
+++ assets/js/shell.js
@@ -0,0 +1,131 @@
+var messages = []
+function Shell(opts){
+    self.max_rows = opts && opts.max_rows ? opts.max_rows : '8';
+    self.ready = false;
+
+
+    return {show: function(message){
+        var left_messages = ['Live Demo:']
+
+        if(message && messages.indexOf(message) == -1){
+            messages.push(message)
+        }
+        if(messages.length > 8){
+            messages.shift()
+        }
+        if(self.ready){
+            var window_width = window.innerWidth
+            var left_margin = window_width /5
+            var logsvg = d3.select("#logs")
+
+            var titles = logsvg.selectAll('text.title').data(left_messages)
+
+            titles.enter()
+            .append('text')
+            .attr('fill', 'red')        
+            .attr('class', 'title')
+            .attr('font-family', 'Share Tech Mono')
+            .attr('font-size', '13px')
+            .attr('x', function(d,i){
+                return left_margin-40;
+            })
+            .attr('y', function(d,i){
+                return 300;
+            })
+            .text(function(d,i){
+                return d
+            })
+            .transition()
+            .attr('y', function(d,i){
+                return 20 + i*15;
+            })
+            .attr('fill', function(d, i){
+            if(d.color){
+                return d.color
+            } else {
+                return 'white';
+            }
+            })
+
+            var logged = logsvg.selectAll('text.logs').data(messages, function(d,i){
+                if(d){
+                    return d.message + new Date()
+                } else {
+                    return new Date()
+                }
+            })
+
+            logged
+            .transition()
+                        .delay(function(d,i){
+                    return i * 100;
+                })
+            .attr('y', function(d,i){
+                return 20 + i*15;
+            })
+            .attr('x', function(d,i){
+                return left_margin+40;
+            })
+            .attr('fill', function(d, i){
+                if(d.color){
+                return d.color
+                } else {
+                return 'white';
+                }
+            })
+
+            logged.enter()
+                .append('text')
+                .attr('fill', 'grey')        
+                .attr('class', 'logs')
+                .attr('font-family', 'Share Tech Mono')
+                .attr('font-size', '13px')
+                .attr('x', function(d,i){
+                return left_margin+40;
+                })
+                .attr('y', function(d,i){
+                return 300;
+                })
+                .text(function(d,i){
+                return d.message
+                })
+                .transition()
+                .delay(function(d,i){
+                    return i * 100;
+                })
+                .attr('y', function(d,i){
+                return 20 + i*15;
+                })
+                .attr('fill', function(d, i){
+                if(d.color){
+                    return d.color
+                } else {
+                    return 'white';
+                }
+                })
+
+            logged.exit()
+            .transition()
+                .attr('y', function(d,i){
+                return -20;
+                })
+                .attr('fill', '#04182b')
+                .remove()
+            d3.select(window)
+                .on("resize", function() {
+                    console.log('resize!')
+                    shell()
+                });
+        }
+        
+    },
+    draw: function(){
+        self.ready = true;
+    },
+    clear: function(){
+        messages = []
+    }
+    }
+ 
+}
+document.Shell = Shell
